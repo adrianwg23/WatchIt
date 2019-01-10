c066d067c4059284cfcf6198f523f8550f698397
adrianwg23 <adrianwg23@hotmail.com>
2019-01-09 23:44:04 -0500

    add BaseLogic class for all logic classes to inherit from

---
+++ app/src/main/java/com/example/adrianwong/watchit/common/BaseLogic.kt
@@ -0,0 +1,9 @@
+package com.example.adrianwong.watchit.common
+
+import com.example.adrianwong.domain.DispatcherProvider
+import kotlinx.coroutines.Job
+
+abstract class BaseLogic(val dispatcher: DispatcherProvider) {
+
+    protected lateinit var jobTracker: Job
+}
