1cc29e4e1778ebd7fdc9cd5d51b8d7fb1c5ccb8f
adrianwg23 <adrianwg23@hotmail.com>
2019-01-09 23:46:30 -0500

    add application class to prepare for dagger

---
+++ app/src/main/java/com/example/adrianwong/watchit/MovieApplication.kt
@@ -0,0 +1,10 @@
+package com.example.adrianwong.watchit
+
+import android.app.Application
+
+class MovieApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
