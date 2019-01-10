5f316de12a1c286b33776ba9678dc740171d929c
adrianwg23 <adrianwg23@hotmail.com>
2019-01-09 23:47:59 -0500

    add DispatcherProvider class for coroutines

---
+++ domain/src/main/java/com/example/adrianwong/domain/DispatcherProvider.kt
@@ -0,0 +1,14 @@
+package com.example.adrianwong.domain
+
+import kotlinx.coroutines.Dispatchers
+import kotlin.coroutines.CoroutineContext
+
+object DispatcherProvider {
+    fun provideUIContext(): CoroutineContext {
+        return Dispatchers.Main
+    }
+
+    fun provideIOContext(): CoroutineContext {
+        return Dispatchers.IO
+    }
+}
