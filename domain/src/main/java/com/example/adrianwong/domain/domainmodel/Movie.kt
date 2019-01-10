2ed9bd637556b8f52e802952c44834a23e3bc603
adrianwg23 <adrianwg23@hotmail.com>
2019-01-09 23:46:02 -0500

    add models in domain and data layer

---
+++ domain/src/main/java/com/example/adrianwong/domain/domainmodel/Movie.kt
@@ -0,0 +1,6 @@
+package com.example.adrianwong.domain.domainmodel
+
+
+class Movie() {
+    // TODO make this a data class later
+}
---
+++ domain/src/main/java/com/example/adrianwong/domain/domainmodel/TvShow.kt
@@ -0,0 +1,5 @@
+package com.example.adrianwong.domain.domainmodel
+
+class TvShow() {
+    // TODO make this a data class later
+}
